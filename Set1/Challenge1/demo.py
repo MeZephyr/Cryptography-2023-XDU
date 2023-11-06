0dbce032b24f3014d56b47061efcfd02f9b5ad7b
ZephyrRyan <ZephyrRyan@outlook.com>
2023-11-06 09:57:00 +0800

    init

---
+++ Set1/Challenge1/demo.py
@@ -0,0 +1,8 @@
+import base64
+
+hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+base64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+base64_string2 = base64.b64encode(hex_string)
+
+print(base64_string2)
